9e334e2df36e88c3dceee416721d68de97f085fb
Rafael Filimberti <rfilimberti@gmail.com>
2025-10-03 08:29:38 -0300

    Adicionando projeto mi_card_flutter corretamente

---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Padding(
+          padding: EdgeInsets.only(bottom: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.teal,
+                child: Center(
+                  child: Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.yellow,
+                  ),
+                ),
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+}
