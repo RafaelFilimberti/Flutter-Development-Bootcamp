e4ae75deab8dbf11ab5e3593ae50dfbe1f396a7d
Rafael Filimberti <rfilimberti@gmail.com>
2025-10-03 15:52:12 -0300

    mycard developer

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -11,33 +11,89 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Padding(
-          padding: EdgeInsets.only(bottom: 20.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
+          child: Stack(
             children: [
               Container(
-                width: 100.0,
-                color: Colors.red,
-              ),
-              Container(
-                color: Colors.teal,
-                child: Center(
-                  child: Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.yellow,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/purple.jpg'),
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-              Container(
-                width: 100.0,
-                color: Colors.blue,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage('images/rafa.jpeg'),
+                  ),
+                  Text(
+                    'Rafa',
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 30.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'FLUTTER DEVELOPER',
+                    style: TextStyle(
+                      fontFamily: 'SourceCodePro',
+                      color: Colors.teal.shade100,
+                      fontSize: 20.0,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                    width: 150.0,
+                    child: Divider(
+                      color: Colors.teal.shade100,
+                    ),
+                  ),
+                  Card(
+                      margin: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 25.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.phone,
+                          color: Colors.teal.shade900,
+                        ),
+                        title: Text(
+                          '+44 123 456 789',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceCodePro',
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      )),
+                  Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal.shade900,
+                      ),
+                      title: Text(
+                        'rfilimberti@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceCodePro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ],
           ),
-        )),
+        ),
       ),
     );
   }
