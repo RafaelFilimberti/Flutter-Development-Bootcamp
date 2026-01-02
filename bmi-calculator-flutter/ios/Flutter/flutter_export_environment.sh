14b294dda95059c51b9afa79509232aa9a9ceb0a
unknown <rfilimberti@gmail.com>
2026-01-02 10:52:28 -0300

    bitcoin-ticker-flutter

--- bmi-calculator-flutter/ios/Flutter/flutter_export_environment.sh
+++ bmi-calculator-flutter/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\FlutterSDK\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Flutter_Bootcamp\Flutter-Development-Bootcamp\bmi-calculator-flutter"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\flutter-bootcamp\Flutter-Development-Bootcamp\bmi-calculator-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
